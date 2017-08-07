d1bbad3d1a7eec2f106635b3d8ceecf7d55c63dc
Chicloon <Chicloon@gmail.com>
2017-08-07 11:36:54 +0900

    .

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = 'localhost:27017';
+const MONGO_URI = 'localhost:27017/lyrical';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
